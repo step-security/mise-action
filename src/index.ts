1f6c3e5cde50e00944bc3232b7a2b9eff22f2764
amanstep <aman@stepsecurity.io>
2025-04-28 09:04:00 +0530

    chore: Added script in action-release.yml and added subscription check

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,28 @@ import * as crypto from 'crypto'
 import * as fs from 'fs'
 import * as os from 'os'
 import * as path from 'path'
+import axios, { isAxiosError } from 'axios'
+
+async function validateSubscription(): Promise<void> {
+  const API_URL = `https://agent.api.stepsecurity.io/v1/github/${process.env.GITHUB_REPOSITORY}/actions/subscription`
+
+  try {
+    await axios.get(API_URL, { timeout: 3000 })
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      core.error(
+        'Subscription is not valid. Reach out to support@stepsecurity.io'
+      )
+      process.exit(1)
+    } else {
+      core.info('Timeout or API not reachable. Continuing to next step.')
+    }
+  }
+}
 
 async function run(): Promise<void> {
   try {
+    await validateSubscription()
     await setToolVersions()
     await setMiseToml()
 
